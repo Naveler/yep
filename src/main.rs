0963e2a09ba7a89760cd6012e555972d7382ef6c
Naveler <georg.vilja@khk.ee>
2022-04-13 16:12:48 +0300

    config struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
     println!("Hello, world!");
+}
+
+struct Config {
+    db_url: String,
+}
+
+impl Config {
+    fn from_env() -> Config {
+        dotenv().ok();
+        return Config {
+            db_url: env::var("DATABASE_URL").unwrap(),
+        };
+    }
 }
